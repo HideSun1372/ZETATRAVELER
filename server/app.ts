9de95e27003b9d2af7010ae052b17abd282b3cc6
Nandery Lolcat <siqichen0802@gmail.com>
2026-06-10 20:52:17 -0700

    Added Render to allowedOrigins array.

--- server/app.ts
+++ server/app.ts
@@ -10,6 +10,7 @@ import express, {
 
 const allowedOrigins = [
   "https://zetatraveler.vercel.app",
+  "https://zetatraveler.onrender.com",
   "http://localhost:5173",
   "http://localhost:5000",
   "http://127.0.0.1:5000",
